96dfaf172603371336ec524496a6557d458ca378
Juan Almeida <92770887+almeidajuan@users.noreply.github.com>
2024-04-30 11:45:58 -0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/calendar_exemple/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calendar_exemple
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/atendimento_model.dart
@@ -0,0 +1,58 @@
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
+import 'dart:collection';
+
+import 'package:table_calendar/table_calendar.dart';
+
+/// Example event class.
+class AtendimentoModel {
+  final String title;
+  final DateTime? startAt;
+  final DateTime? endAt;
+
+  const AtendimentoModel(this.title, this.startAt, this.endAt);
+
+  @override
+  String toString() => title;
+
+  Duration get duration => endAt!.difference(startAt!);
+  int get durationInMinutes => duration.inMinutes;
+  int get eventSize => durationInMinutes ~/ 15;
+}
+
+/// Example events.
+///
+/// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
+// final kEvents = LinkedHashMap<DateTime, List<AtendimentoModel>>(
+//   equals: isSameDay,
+//   hashCode: getHashCode,
+// )..addAll(_kEventSource);
+
+// final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
+//     key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
+//     value: (item) => List.generate(item % 4 + 1,
+//         (index) => AtendimentoModel('Event $item | ${index + 1}')))
+//   ..addAll({
+//     kToday: [
+//       AtendimentoModel('Today\'s Event 1'),
+//       AtendimentoModel('Today\'s Event 2'),
+//     ],
+//   });
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
+}
+
+/// Returns a list of [DateTime] objects from [first] to [last], inclusive.
+List<DateTime> daysInRange(DateTime first, DateTime last) {
+  final dayCount = last.difference(first).inDays + 1;
+  return List.generate(
+    dayCount,
+    (index) => DateTime.utc(first.year, first.month, first.day + index),
+  );
+}
+
+final kToday = DateTime.now();
+final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
